9b06198b4bc2659a2df1cf2759923f02f001706a
Jam Risser <jam@codejam.ninja>
2019-12-06 04:27:19 +0000

    Patched data

--- src/reduxCache.ts
+++ src/reduxCache.ts
@@ -1,42 +1,47 @@
+import { Cache } from 'apollo-cache';
+import { DepTrackingCache } from 'apollo-cache-inmemory/lib/depTrackingCache';
+import { ObjectCache } from 'apollo-cache-inmemory/lib/objectCache';
+import { cloneDeep } from 'lodash';
 import {
-    Cache,
-} from 'apollo-cache';
-import {
-    ApolloReducerConfig,
-    InMemoryCache,
-    writeResultToStore
+  ApolloReducerConfig,
+  InMemoryCache,
+  NormalizedCache,
+  StoreWriter
 } from 'apollo-cache-inmemory';
-
 import {
-    ReduxNormalizedCacheConfig,
-    reduxNormalizedCacheFactory
+  ReduxNormalizedCacheConfig,
+  reduxNormalizedCacheFactory
 } from './reduxNormalizedCache';
-import {cloneDeep} from 'lodash';
 
 export type ReduxCacheConfig = ApolloReducerConfig & ReduxNormalizedCacheConfig;
 
 export class ReduxCache extends InMemoryCache {
-    constructor(config: ReduxCacheConfig) {
-        super(config);
-        // Overwrite the in-memory data object
-        this.data = reduxNormalizedCacheFactory(config);
-    }
+  constructor(config: ReduxCacheConfig) {
+    super(config);
+    // Overwrite the in-memory data object
+    // @ts-ignore
+    this.data = reduxNormalizedCacheFactory(config);
+  }
 
-    public write(write: Cache.WriteOptions): void {
-        const data = this.config.storeFactory(cloneDeep(this.data.toObject()));
-
-        writeResultToStore({
-            dataId: write.dataId,
-            result: write.result,
-            variables: write.variables,
-            document: this.transformDocument(write.query),
-            store: data,
-            dataIdFromObject: this.config.dataIdFromObject,
-            fragmentMatcherFunction: this.config.fragmentMatcher.match,
-        });
-
-
-        this.data.replace(data.toObject());
-        this.broadcastWatches();
-    }
+  public write(write: Cache.WriteOptions): void {
+    // @ts-ignore
+    const this_data: NormalizedCache = this.data;
+    // @ts-ignore
+    const this_storeWriter: StoreWriter = this.storeWriter;
+    // const data = this.config.storeFactory(cloneDeep(this_data.toObject()));
+    const data = this.config.resultCaching
+      ? new DepTrackingCache(cloneDeep(this_data.toObject()))
+      : new ObjectCache(cloneDeep(this_data.toObject()));
+    this_storeWriter.writeResultToStore({
+      dataId: write.dataId,
+      result: write.result,
+      variables: write.variables,
+      document: this.transformDocument(write.query),
+      store: data,
+      dataIdFromObject: this.config.dataIdFromObject,
+      fragmentMatcherFunction: this.config.fragmentMatcher.match
+    });
+    this_data.replace(data.toObject());
+    this.broadcastWatches();
+  }
 }
